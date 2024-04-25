2b0464a937b593417e9b7a0597bcd1e7eae7c96e
Carlos Cesar Martins Ferreira <162916369+carloscesarferreira@users.noreply.github.com>
2024-04-25 07:56:24 +0100

    Add files via upload

---
+++ Introduction and Basic Concepts/video03_install_test_versions.py
@@ -0,0 +1,41 @@
+# Check the versions of libraries
+
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# seaborn
+import seaborn
+print('seaborn: {}'.format(seaborn.__version__))
+# plotly
+import plotly
+print('plotly: {}'.format(plotly.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+# tensorflow
+import tensorflow
+print('tensorflow: {}'.format(tensorflow.__version__))
+# torch
+import torch
+print('torch: {}'.format(torch.__version__))
+# nltk
+import nltk
+print('nltk: {}'.format(nltk.__version__))
+# spacy
+import spacy
+print('spacy: {}'.format(spacy.__version__))
+# gensim
+import gensim
+print('gensim: {}'.format(gensim.__version__))
